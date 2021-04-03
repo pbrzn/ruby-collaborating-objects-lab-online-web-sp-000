15c2fa9581a0f6a30a3cbadcfb59a5e12e640b50
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-04-03 02:41:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,14 +20,14 @@ class Song
   end
   
   def artist_name=(name)
-    #if self.artist == nil
+    if self.artist == nil
       result = Artist.all.find {|artist| artist.name==name}
       if result == name
         self.artist.name = name
       else
         self.artist = Artist.new(name)
       end
-    #end
+    end
   end
   
 end
