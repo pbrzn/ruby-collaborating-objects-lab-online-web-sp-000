94f22a5c8c046e501aaa311a16728723c76a832b
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-04-03 02:29:43 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,8 +22,7 @@ class Artist
   end
   
   def self.find_or_create_by_name(name)
-    self.all.find {|artist|
-      artist.name == name artist : self.new(name)
+    self.all.find {|artist| artist.name == name}
     end
   end
 
